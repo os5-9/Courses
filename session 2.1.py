ac58e8044a9821708eb8ca74cd45732abd168db9
salavat876 <mavlikhanov2002@mail.ru>
2022-08-05 16:56:25 +0400

    lesson 1.2 and 2.1

---
+++ session 2.1.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+import math
+
+
+def calc(z):
+  return str(math.log(abs(12*math.sin(int(z)))))
+
+
+driver = webdriver.Chrome()
+driver.get("https://suninjuly.github.io/math.html")
+
+x_element = driver.find_element_by_id("input_value")
+x = x_element.text
+print("x = " + x)
+
+answer = calc(x)
+print("the answer is " + str(answer))
+
+answer_box = driver.find_element_by_id("answer")
+answer_box.send_keys(answer)
+
+driver.find_element_by_css_selector("[for='robotCheckbox']").click()
+driver.find_element_by_css_selector("[for='robotsRule']").click()
+driver.find_element_by_css_selector("[type='submit']").click()
