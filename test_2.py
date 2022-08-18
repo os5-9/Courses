077d6ff3b5735c6279ebfe885559e9cb6bf48626
os <os.5-9@yandex.ru>
2022-08-18 21:10:50 +0400

    work with fixtures

--- test_2.py
+++ test_2.py
@@ -6,7 +6,7 @@ class TestAbs(unittest.TestCase):
         self.assertEqual(abs(-42), 42, "Should be absolute value of a number")
 
     def test_abs2(self):
-        self.assertEqual(abs(-42), -42, "Should be absolute value of a number")
+        self.assertEqual(abs(-2), 2, "Should be absolute value of a number")
 
 
 if __name__ == "__main__":
---
+++ test_fixture3.py
@@ -0,0 +1,26 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+link = "https://suninjuly.github.io/math.html"
+
+
+@pytest.fixture()
+def driver():
+    print("\n STARTING WEBDRIVER")
+    browser = webdriver.Chrome()
+    return browser
+
+
+class TestCaseXValue:
+    def test_x_value_more_than_zero(self, driver):
+        driver.get(link)
+        x_element = driver.find_element(By.ID, "input_value")
+        x = int(x_element.text)
+        assert x > 0, "x value must be more than 0"
+
+    def test_x_value_less_than_thousand(self, driver):
+        driver.get(link)
+        x_element = driver.find_element(By.ID, "input_value")
+        x = int(x_element.text)
+        assert x < 1000, "x value must be less than 1000"
